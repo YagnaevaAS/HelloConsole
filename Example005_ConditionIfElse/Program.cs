760301744256c6de1dfef419d5be09de8ff78c20
Alina Yagnaeva <as.yagnaeva97@gmail.com>
2023-10-18 16:50:05 +0300

    add example 005 if-else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Мария, добрый деннь, рады вас видеть!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
